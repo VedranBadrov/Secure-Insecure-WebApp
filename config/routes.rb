083bff8a208060edee4289a4a279689637ce72a4
unknown <1902badrovvedran@gmail.com>
2023-03-24 14:33:31 +0000

    Secure Authentication & Access control

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/", to: "page#home"
+  get "/All", to: "pages#all"
+  get "/Managers&Admin", to: "pages#MA"
+  get "/Admin", to: "pages#A"
 end
