930d295817d7c435df82de1d1e6e0cee0194da73
unknown <1902badrovvedran@gmail.com>
2023-03-30 21:35:04 +0100

    Inital commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "/", to: "page#home"
+  get "/All", to: "pages#all"
+  get "/Managers&Admin", to: "pages#MA"
+  get "/Admin", to: "pages#A"
   # Defines the root path route ("/")
   # root "articles#index"
 end
