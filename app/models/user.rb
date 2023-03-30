7f129955c6bd638a17eec0d4347d57414f574976
unknown <1902badrovvedran@gmail.com>
2023-03-30 16:49:32 +0100

    Access control

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,16 +6,6 @@ class User < ApplicationRecord
   
   validates :email, :password, presence: true
   validates :password, length: { minimum: 8 }
-  validate :email_password_combination
-
-  def email_password_combination
-    user = User.find_by(email: email)
-    if user && !user.authenticate(password)
-      errors.add(:password, 'is incorrect')
-    elsif !user
-      errors.add(:email, 'does not exist')
-    end
-  end
 
   enum role: [:USER, :MODERATOR, :ADMIN]
   after_initialize :set_default_role, :if => :new_record?
