28a466e3321d549dc701ba1a8d8c3bd49c40248d
unknown <1902badrovvedran@gmail.com>
2023-03-31 11:20:50 +0100

    Broken access control

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :email, :password, presence: true
+  validates :password, length: { minimum: 8 }
+       
+  enum role: [:USER, :MODERATOR, :ADMIN]
+  after_initialize :set_default_role, :if => :new_record?
+  def set_default_role
+    self.role ||= :user
+  end
+end
